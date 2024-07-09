53cc99834e2be9987f233ab6c1c1012a8f64574e
taewoo922 <taewoo922@gmail.com>
2024-07-09 14:12:30 +0900

    login, join

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -1,8 +1,23 @@
 package com.cod.market.member.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
-
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "로그인 페이지입니다.";
+    }
+    @GetMapping("/join")
+    @ResponseBody
+    public String join() {
+        return "회원가입 페이지입니다.";
+    }
 }
