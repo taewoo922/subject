b3f77db07760da51b00106c43aab8d467db850bb
taewoo922 <taewoo922@gmail.com>
2024-07-09 16:14:33 +0900

    join form

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.cod.market.member.controller;
 
+import com.cod.market.member.form.MemberForm;
+import com.cod.market.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,12 +14,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
     @GetMapping("/login")
     public String login() {
         return "member/login";
     }
+
     @GetMapping("/join")
-    public String join() {
+    public String showJoin() {
         return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid MemberForm memberForm) {
+        memberService.join(memberForm.getUsername(),memberForm.getPassword(),memberForm.getNickname(),memberForm.getEmail());
+        return "redirect:/member/login";
     }
 }
